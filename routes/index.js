1da764afd7ef672413ff85c7de7624fa0bb03127
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2017-02-17 22:38:35 -0800

    finding first food item

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ var router  = express.Router()
 router.post('/start', function (req, res) {
   // NOTE: Do something here to start the game
 
+  //console.log(req.body);
+
   // Response data
   var data = {
     color: "#DFFF00",
-    name: "Trump Snake",
+    name: "The Intimidating Saj",
     head_url: "http://www.placecage.com/c/200/200", // optional, but encouraged!
     taunt: "Let's do thisss thang!", // optional, but encouraged!
   }
@@ -20,10 +22,59 @@ router.post('/start', function (req, res) {
 router.post('/move', function (req, res) {
   // NOTE: Do something here to generate your move
 
+  var gen_move;
+
+  console.log(req.body.snakes[0].coords[0]); //contains the coordinates of the snakes' coordinates
+  console.log(req.body.food);
+
+  function check_if_edge() {
+    //top left corner of wall
+    if(req.body.snakes[0].coords[0][0] == 0 && req.body.snakes[0].coords[0][1] == 0) {
+      gen_move = 'right';
+    }
+    //top right corner of wall
+    else if(req.body.snakes[0].coords[0][0] == 19 && req.body.snakes[0].coords[0][1] == 0) {
+      gen_move = 'down';
+    }
+    //bottom right corner of wall
+    else if(req.body.snakes[0].coords[0][0] == 19 && req.body.snakes[0].coords[0][1] == 19) {
+      gen_move = 'left';
+    }
+    //bottom left corner of wall
+    else if(req.body.snakes[0].coords[0][0] == 0 && req.body.snakes[0].coords[0][1] == 19) {
+      gen_move = 'up';
+    }
+  }
+
+  function check_if_foodrightleft() {
+    //if food is to the right somewhere close
+    if(req.body.snakes[0].coords[0][0] >= req.body.food[0][0]) {
+      gen_move = 'left';
+    }
+    //if food is to the left somewhere close
+    else if(req.body.snakes[0].coords[0][0] <= req.body.food[0][0]) {
+      gen_move = 'right';
+    }
+  }
+
+  function check_if_topwall() {
+    //if we reach the top
+    if(req.body.snakes[0].coords[0][1] == 0) {
+      //do something to make a smart choice of left or right
+      //remove this line
+      gen_move = 'left';
+    }
+  }
+
+  check_if_edge();
+  check_if_foodrightleft();
+  check_if_topwall();
+
   // Response data
   var data = {
-    move: 'up', // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
+    //one of: ['up','down','left','right']
+    move: gen_move,
+    taunt: 'Snake and bake', // optional, but encouraged!
   }
 
   return res.json(data)
